3dbd99723f62523aa886f18f9164b64bce528d93
JongwooYoun <swk06480@icloud.com>
2023-12-20 09:38:21 +0900

    Server_ : 파일 정리

--- server_test/Assets/Resource/Script/UDP_Server.cs
+++ server_test/Assets/Resource/Script/UDP_Server.cs
@@ -5,56 +5,47 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UDPServer : MonoBehaviour
-{
+public class UDPServer : MonoBehaviour{
     public Text serverStatusText;
     public Text receivedMessageText;
 
     private UdpClient udpServer;
     private IPEndPoint remoteEndPoint;
 
-    private void Start()
-    {
+    private void Start(){
         udpServer = new UdpClient(5555);
         remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
         UpdateServerStatus("Server started on port 5555");
     }
 
-    private void Update()
-    {
-        try
-        {
-            if (udpServer.Available > 0)
-            {
+    private void Update(){
+        try{
+            if (udpServer.Available > 0){
                 byte[] data = udpServer.Receive(ref remoteEndPoint);
                 string message = Encoding.UTF8.GetString(data);
                 Debug.Log("Received: " + message);
 
-                // GUI¿¡ ¼ö½ÅÇÑ ¸Ş½ÃÁö Ç¥½Ã
+                // GUIì— ìˆ˜ì‹ í•œ ë©”ì‹œì§€ í‘œì‹œ
                 UpdateReceivedMessage(message);
 
-                // ¿©±â¿¡¼­ ¹ŞÀº ÀÔ·Â Á¤º¸(message)¸¦ Ã³¸®ÇÏ¸é µË´Ï´Ù.
+                // ì—¬ê¸°ì—ì„œ ë°›ì€ ì…ë ¥ ì •ë³´(message)ë¥¼ ì²˜ë¦¬í•˜ë©´ ë©ë‹ˆë‹¤.
             }
         }
-        catch (Exception e)
-        {
+        catch (Exception e){
             Debug.LogError("Error receiving data: " + e.Message);
         }
     }
 
-    private void UpdateServerStatus(string status)
-    {
+    private void UpdateServerStatus(string status){
         serverStatusText.text = "Server Status: " + status;
     }
 
-    private void UpdateReceivedMessage(string message)
-    {
+    private void UpdateReceivedMessage(string message){
         receivedMessageText.text = "Received Message: " + message;
     }
 
-    private void OnDestroy()
-    {
+    private void OnDestroy(){
         if (udpServer != null)
             udpServer.Close();
     }
