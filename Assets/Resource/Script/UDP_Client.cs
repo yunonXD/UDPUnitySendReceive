eff83252a3134ba0d72beb58e4005ab25ed8b43f
Yunon <lewdmint@protonmail.com>
2023-12-27 22:19:11 +0900

    Client_KeyDown and KeyUp logic fuction working now

--- Assets/Resource/Script/UDP_Client.cs
+++ Assets/Resource/Script/UDP_Client.cs
@@ -9,6 +9,7 @@ public class UDP_Client : MonoBehaviour{
     private const string serverIP = "192.168.0.54"; // 서버의 IP 주소
     private const int serverPort = 9020;        // 서버의 포트 번호
     private UdpClient udpClient;
+    bool wasAnyKeyPreviouslyPressed = false;        //키 입력 여부 false=눌림 true=들림
 
 
     [SerializeField] Text ClientState;
@@ -21,41 +22,59 @@ public class UDP_Client : MonoBehaviour{
     }
 
     void Update(){
-        // 사용자의 입력을 받아서 서버에 전송
-        // if (Input.anyKey){ 
-        //     SendKeyTable(Input.inputString.ToUpper()); // 대문자로 변환      
+        // if(Input.anyKeyDown){
+        //     Debug.Log("키 눌림");
+        //     string pressedKeys = GetPressedKeys();
+        //     //SendKeyTable(pressedKeys);
+        // }
+        // else if(!Input.anyKeyDown){
+        //     Debug.Log("키 때짐");
+        //     string pressedkeysUp = GetPressedKeys();
+        //     //SendKeyTable(pressedKeys);
         // }
 
-        if(Input.anyKeyDown){
-            string pressedKeys = GetPressedKeys();
-            SendKeyTable(pressedKeys);
+        if (Input.anyKey){
+            // 아무 키가 눌린 상태에서만 실행되는 코드 작성
+            if (!wasAnyKeyPreviouslyPressed){
+                string pressedKeys = GetPressedKeys();
+                SendKeyTable(pressedKeys);
+            }
+            wasAnyKeyPreviouslyPressed = true;
+        }
+        else{
+            // 아무 키가 놓예진 상태에서만 실행되는 코드 작성
+            if (wasAnyKeyPreviouslyPressed){
+                string pressedKeys = GetPressedKeys();
+                SendKeyTable(pressedKeys);
+            }
+            wasAnyKeyPreviouslyPressed = false;
         }
     }
 
     string GetPressedKeys(){
     StringBuilder pressedKeys = new StringBuilder();
 
-    // F1부터 F12까지 검사
-    for(KeyCode keyCode = KeyCode.F1; keyCode <= KeyCode.F12; keyCode++){
-        if(Input.GetKey(keyCode)){
+        // F1부터 F12까지 검사
+        for(KeyCode keyCode = KeyCode.F1; keyCode <= KeyCode.F12; keyCode++){
+            if(Input.GetKey(keyCode)){
             pressedKeys.Append(keyCode.ToString());
+            }
         }
-    }
 
-    // Alt 키 검사
-    if(Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)){
-        pressedKeys.Append("Alt");
-    }
-
-    // 나머지 키 검사
-    foreach(char c in Input.inputString){
-        if(c != '\0'){
-            pressedKeys.Append(c.ToString().ToUpper());
+        // Alt 키 검사
+        if(Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)){
+            pressedKeys.Append("Alt");
         }
-    }
 
-    return pressedKeys.ToString();
-}
+        // 나머지 키 검사
+        foreach(char c in Input.inputString){
+            if(c != '\0'){
+                pressedKeys.Append(c.ToString().ToUpper());
+            }
+        }
+
+        return pressedKeys.ToString();
+    }
 
     
 
@@ -74,21 +93,38 @@ public class UDP_Client : MonoBehaviour{
 
    private async void SendKeyTable(string keyName){
     try{
+   
         // 키 테이블 가져오기
         if (KeyTables.keyTableDictionary.TryGetValue(keyName, out var keyTable)){
 
-            // 데이터 초기화
-            Array.Clear(keyTable.make_str, 0, keyTable.make_str.Length);
+            if(!wasAnyKeyPreviouslyPressed){        //키 down 상태일때
 
-            // keyTable의 make_str을 바이트 배열로 변환하여 서버로 전송
-            keyTable.make_str_len = make_key_string(keyTable.make_str, keyTable.make_val);
-            byte[] data = keyTable.make_str;
+                // make_str 데이터 초기화
+                Array.Clear(keyTable.make_str, 0, keyTable.make_str.Length);
+                // keyTable의 make_str을 바이트 배열로 변환하여 서버로 전송
+                keyTable.make_str_len = make_key_string(keyTable.make_str, keyTable.make_val);
+                byte[] data = keyTable.make_str;
 
-            await udpClient.SendAsync(data, keyTable.make_str_len);     //이 부분이 문제였다! 
-            //Debug.Log($"Sent make_str for key {keyName}: {ByteArrayToString(data)}");
-            ClientText.text = $"Sent make_str for key {keyName}: {ByteArrayToString(data)}";
-            keyTable.make_str_len=0;
-        }
+                await udpClient.SendAsync(data, keyTable.make_str_len);
+
+                ClientText.text = $"Sent make_str for key {keyName}: {ByteArrayToString(data)}";
+                keyTable.make_str_len=0;
+
+            }
+            else if(wasAnyKeyPreviouslyPressed){    //키 up 상태일때
+
+                // break_str 데이터 초기화
+                Array.Clear(keyTable.break_str, 0, keyTable.break_str.Length);
+                // keyTable의 break_str을 바이트 배열로 변환하여 서버로 전송
+                keyTable.break_str_len = make_key_string(keyTable.break_str, keyTable.break_val);
+                byte[] data = keyTable.break_str;
+
+                await udpClient.SendAsync(data, keyTable.break_str_len);
+
+                ClientText.text = $"Sent break_str for key {keyName}: {ByteArrayToString(data)}";
+                keyTable.break_str_len=0;
+            }
+        }                
     }
     catch (Exception e){
         Debug.LogError($"Failed to send KeyTable data: {e.Message}");
