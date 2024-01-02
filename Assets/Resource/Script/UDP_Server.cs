4180547fbe9b541fb13f2870a88a1fd3390a1d54
JongwooYoun <swk06480@icloud.com>
2024-01-02 17:55:54 +0900

    Server_스크립트 단축(ProcessReceivedData) 부분

--- Assets/Resource/Script/UDP_Server.cs
+++ Assets/Resource/Script/UDP_Server.cs
@@ -4,7 +4,8 @@ using System.Net.Sockets;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using System.Linq;
+
+using UnityEngine.EventSystems;
 
 public class UDPServer : MonoBehaviour{
 
@@ -14,6 +15,8 @@ public class UDPServer : MonoBehaviour{
     [SerializeField] private TextMeshProUGUI receivedMessageText;
     [SerializeField] private int m_Port =9020;
     [SerializeField] private bool isHangulMode =false;      //한글유무
+
+    [SerializeField] private EventSystem eventSystem;
 
 
     private UdpClient udpServer;
@@ -67,60 +70,34 @@ public class UDPServer : MonoBehaviour{
 
 private Queue<byte[]> cumulativeDataQueue = new Queue<byte[]>(); // 모든 데이터를 누적하는 큐
 
-private void ProcessReceivedData(){
-    while (receivedDataQueue.Count > 0){
+private void ProcessReceivedData() {
+    while (receivedDataQueue.Count > 0) {
         byte[] newData = receivedDataQueue.Dequeue(); // 새로운 데이터를 큐에서 가져옴
 
-        // 큐에 있던 데이터와 합쳐서 새로운 큐를 만듦
-        Queue<byte[]> combinedQueue = new Queue<byte[]>(cumulativeDataQueue);
-        combinedQueue.Enqueue(newData);
-
         // 바이트 데이터에서 바이트가 들어있는 부분을 카운트하고 출력
-        string accumulatedMessage = "";
-        foreach (byte[] data in combinedQueue) {
-            int byteCount = 0;
-            for (int i = 0; i < data.Length; i++) {
-                if (data[i] != 0x00) {
-                    byteCount++;
+        string partialMessage = Input.compositionString;
+        foreach (byte b in newData) {
+            if (b != 0x00) {
+                int asciiValue = b;
+
+                if (isHangulMode && KeyTables.KeyTableForLong.ContainsKey(asciiValue)) {
+                    // 한글 입력 상태에서 키를 눌렀을 때 한글로 변환하여 부분 메시지에 추가
+                    KeyTable keyTable = KeyTables.KeyTableForLong[asciiValue];
+                    partialMessage += keyTable.KRname;
                 } else {
-                    break;
-                }
-            }
-
-            if (byteCount >= 1) {
-                // 바이트 배열의 값을 아스키 코드로 변환하여 누적된 메시지에 추가
-                for (int i = 0; i < byteCount; i++) {
-                    int asciiValue = data[i];
-
-                    if(isHangulMode && KeyTables.KeyTableForLong.ContainsKey(asciiValue)){
-                        // 한글 입력 상태에서 키를 눌렀을 때 한글로 변환하여 누적된 메시지에 추가                     
-                        KeyTable keyTable = KeyTables.KeyTableForLong[asciiValue];
-                        accumulatedMessage +=keyTable.KRname;
-                    }
-                    else{
-                        KeyTable keyTable = KeyTables.KeyTableForLong[asciiValue];
-                        accumulatedMessage +=keyTable.name;
-                    }
-
-                    //if(asciiValue == KeyTables.keyTableDictionary["change"].make_val){
-                        // 한/영 키를 눌렀을 때 한글 입력 상태를 변경                       
-                    //}
-
+                    KeyTable keyTable = KeyTables.KeyTableForLong[asciiValue];
+                    partialMessage += keyTable.name;
                 }
             } else {
-                Debug.LogWarning("Received Data is not sufficient to convert to Decimal.");
+                // 0x00은 데이터의 끝을 나타내므로, 반복문 종료
+                break;
             }
         }
 
         // 출력 업데이트
-        receivedMessageText.text = "Received Message: " + accumulatedMessage;
-        inputfieldTest.text="";
-        inputfieldTest.text +=accumulatedMessage;
-        //inputfieldTest.selectionStringAnchorPosition = accumulatedMessage.Length;
-        //inputfieldTest.selectionStringFocusPosition = accumulatedMessage.Length;
+        receivedMessageText.text = "Received Message: " + partialMessage;
+        inputfieldTest.text += partialMessage;
 
-        // 누적된 큐를 갱신
-        cumulativeDataQueue = new Queue<byte[]>(combinedQueue);
     }
 }
 
